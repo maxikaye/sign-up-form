0893005c3fc253623cc0188100795daf656003d9
maxikaye <mr.max.kaye@gmail.com>
2022-11-09 12:55:03 -0500

    Add JS password matching validation

---
+++ validate.js
@@ -0,0 +1,18 @@
+let passwordField = document.getElementById("password");
+let passwordConfirmField = document.getElementById("password-confirm");
+let userPassword, userPasswordConfirm;
+const passwordError = document.querySelector(".error");
+
+
+passwordField.addEventListener('input', e => {
+    userPassword = e.target.value;
+})
+
+passwordConfirmField.addEventListener('input', e => {
+    userPasswordConfirm = e.target.value;
+    if (userPasswordConfirm !== userPassword) {
+        passwordError.style.setProperty('visibility', 'visible');
+    } else {
+        passwordError.style.setProperty('visibility', 'hidden');
+    }
+})
